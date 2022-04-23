bbdb63aa992de0c5c1e8c3de48e71cb3b0bc8fee
Zach Moshe <zmoshe@snapchat.com>
2022-04-23 15:00:28 +0300

    adding daytime/nighttime modes to the controller.
    controller is now in charge of choosing the random song to play.

--- src/audio_library.py
+++ src/audio_library.py
@@ -1,25 +1,11 @@
 import asyncio
-from asyncio import subprocess
 import logging
-import pathlib
-import random 
 
 import alsaaudio
 
 
 APLAY_BINARY = "aplay"
 
-def _parse_glob_expressions(glob_expressions):
-    return list(
-        song_filename 
-        for g in glob_expressions
-        for song_filename in pathlib.Path().glob(g))
-
-def _pick_random(audio_list, last_played):
-    # pick a random one but not the last one (if len(_songs) > 1)
-    valid_songs = [x for x in audio_list if len(audio_list) == 1 or last_played is None or x != last_played]
-    random_filename = random.choice(valid_songs)
-    return random_filename
 
 async def _terminate_or_kill_procs(procs):
     for proc in procs:
@@ -35,25 +21,10 @@ async def _terminate_or_kill_procs(procs):
 
 class AudioLibrary:
     def __init__(self, config):
-        self._songs = _parse_glob_expressions(config["songs"])
-        self._last_played_song = None
-
-        self._yaar_callings = _parse_glob_expressions(config["animals"]["yaar_callings"])
-        self._animals = _parse_glob_expressions(config["animals"]["animals_sounds"])
-        self._last_played_animal = None
-
         self._currently_playing_task = None
         self._currently_playing_filename = None
-
-        if not self._songs:
-            raise ValueError("parsed songs list is empty.")
-        if not self._yaar_callings:
-            raise ValueError("parsed yaar_callings list is empty.")
-        if not self._animals:
-            raise ValueError("parsed animal_sounds list is empty.")
-
         self.mixer = alsaaudio.Mixer(control="Master")
-        self.volume_step = config.get("volume_step", 10)
+        self.volume_step = config["volume_step"]
 
     def get_currently_playing_filename(self):
         return self._currently_playing_filename
@@ -105,19 +76,13 @@ class AudioLibrary:
         self.stop_audio()
         self._currently_playing_task = asyncio.create_task(self._play_sound_file("recordings/sounds/shutdown.wav"))
 
-    def play_random_song(self):
+    def play_random_song(self, filename):
         self.stop_audio()
-        random_filename = _pick_random(self._songs, self._last_played_song)
-        self._last_played_song = random_filename
-        self._currently_playing_task = asyncio.create_task(self._play_sound_file(random_filename))
+        self._currently_playing_task = asyncio.create_task(self._play_sound_file(filename))
 
-    def play_random_animal_sound(self):
+    def play_random_animal_sound(self, animal_filename, calling_filename):
         self.stop_audio()
-        # last_played is by the animal. The calling will be randomly chosen regardless.
-        random_animal_filename = _pick_random(self._animals, self._last_played_animal)
-        self._last_played_animal = random_animal_filename
-        random_calling_filename = random.choice(self._yaar_callings)
-        self._currently_playing_task = asyncio.create_task(self._play_animal(random_animal_filename, random_calling_filename))
+        self._currently_playing_task = asyncio.create_task(self._play_animal(animal_filename, calling_filename))
 
     def stop_audio(self):
         if self._currently_playing_task is not None:
