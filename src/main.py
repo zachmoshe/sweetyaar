fe248c707efcd0ef742244c6852cccdc60c94d0b
Zach Moshe <zmoshe@snapchat.com>
2022-04-25 20:30:15 +0300

    switching to logrotate

--- src/main.py
+++ src/main.py
@@ -1,23 +1,26 @@
-import asyncio
-import atexit
 from cgitb import lookup
 import logging
-import pathlib
+from logging.handlers import TimedRotatingFileHandler
 import pprint 
-import sys
 
-import audio_library
 import controller
 import yaml
 
 
 _CONFIG_FILE = "etc/config.yaml"
 
-logging.basicConfig(
-     level=logging.DEBUG, 
-     format= '[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s',
-     datefmt='%Y-%m-%d %H:%M:%S'
- )
+
+def create_timed_rotating_log(path, log_level_str):
+    logger = logging.getLogger()
+    logger.setLevel(logging.getLevelName(log_level_str))
+    
+    # Every day. Keep 30 days back.
+    handler = TimedRotatingFileHandler(path,
+                                       when="D",
+                                       interval=1,
+                                       backupCount=30)
+    handler.setFormatter(logging.Formatter("[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s"))
+    logger.addHandler(handler)
 
 
 def load_config(config_file):
@@ -26,6 +29,7 @@ def load_config(config_file):
 
 def main() -> int:
     cfg = load_config(_CONFIG_FILE)
+    create_timed_rotating_log(cfg["log_path"], cfg["log_level"])
     logging.info("Starting sweetyaar control loop with the following config:\n" + pprint.pformat(cfg))
     ctl = controller.SweetYaarController(cfg)
     ctl.take_control()
