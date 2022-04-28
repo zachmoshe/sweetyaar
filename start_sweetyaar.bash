c7c7241d5c215358fac760714c8ef4d88e6408a0
Zach Moshe <zmoshe@snapchat.com>
2022-04-28 20:04:07 +0300

    starting sweetyaar on boot and rebooting if the main program exits.

--- start_sweetyaar.bash
+++ start_sweetyaar.bash
@@ -2,3 +2,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd $SCRIPT_DIR
 authbind --deep poetry run python src/main.py 
+
+# On exit reboot the device to allow for catastrophic reboots if needed.
+sudo shutdown -r now
