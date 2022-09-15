7c652e62ea72486ba65b042d1a93cab73fbc4c7f
Zach Moshe <zachmoshe@gmail.com>
2022-09-15 13:40:05 +0300

    changing audio_player to have a constantly running loop.

--- src/audio_player.py
+++ src/audio_player.py
@@ -6,10 +6,25 @@ from machine import I2S, Pin
 EVENT_AUDIO_STARTED = 1
 EVENT_AUDIO_FINISHED = 2
 
+WAV_SAMPLE_BITS = 16
+WAV_NUM_CHANNELS = 2
+WAV_SAMPLE_RATE = 16000
+
 
 def _assert_wav_file(filename):
     if not filename.lower().endswith(".wav"):
         raise ValueError(f"Illegal filename '{filename}'. Only WAVs are supported.")
+    
+    # Read WAV properties
+    with wave.open(filename) as f:
+        wav_frame_rate = f.getframerate()
+        wav_num_channels = f.getnchannels()
+        wav_sample_width_bits = 8 * f.getsampwidth()
+    
+        if (wav_frame_rate != WAV_SAMPLE_RATE) or (wav_num_channels != WAV_NUM_CHANNELS) or (wav_sample_width_bits != WAV_SAMPLE_BITS):
+            raise ValueError(
+                f"Illegal WAV file. Expected (sample_rate={WAV_SAMPLE_RATE}, {WAV_NUM_CHANNELS} channels x {WAV_SAMPLE_BITS} bits/sample), Got (sample_rate={wav_frame_rate}, {wav_num_channels} channels x {wav_sample_width_bits} bits/sample).")
+
 
 
 class AudioPlayer:
@@ -23,10 +38,73 @@ class AudioPlayer:
         self.activity_listener_callbacks = []
 
         self._currently_playing_task = None
-        self._currently_playing_desc = ""
+        self._input_file_handle = None
+        self._silent_samples = bytearray(512)
+        self._wav_samples = memoryview(bytearray(self.buffer_length_bytes))
 
-        wav_samples = bytearray(self.buffer_length_bytes)
-        self.wav_samples_mv = memoryview(wav_samples)
+        self.audio_out = I2S(
+            self.i2s_id,
+            sck=Pin(self.gpio_sck),
+            ws=Pin(self.gpio_ws),
+            sd=Pin(self.gpio_sd),
+            mode=I2S.TX,
+            bits=WAV_SAMPLE_BITS,
+            format=I2S.STEREO,
+            rate=WAV_SAMPLE_RATE,
+            ibuf=self.buffer_length_bytes,
+        ) 
+        asyncio.create_task(self._play_loop())
+
+
+    async def _play_loop(self):
+        swriter = asyncio.StreamWriter(self.audio_out)
+
+        while True:
+            try:
+                if self._input_file_handle is None:
+                    swriter.out_buf = self._silent_samples
+                    await swriter.drain()
+
+                else:
+                    for _ in range(3):
+                        num_read = 0
+                        try:
+                            num_read = self._input_file_handle.readinto(self._wav_samples)
+                            break 
+                        except Exception as e:
+                            print("EXCEPTION IN PLAYER (READ): ", e)
+                            print(e)
+                            
+                    if num_read == 0:  # EOF or 3 errors while reading
+                        self.stop()
+                    else:
+                        # apply temporary workaround to eliminate heap allocation in uasyncio Stream class.
+                        # workaround can be removed after acceptance of PR:
+                        #    https://github.com/micropython/micropython/pull/7868
+                        # swriter.write(wav_samples_mv[:num_read])
+                        swriter.out_buf = self._wav_samples[:num_read]
+                        await swriter.drain()
+            except Exception as e:
+                print("EXCEPTION IN PLAYER: ", e)
+                print(e)
+                self.stop()
+
+
+    def play_file(self, wav_filename, desc=""):
+        _assert_wav_file(wav_filename)
+        self.stop()  # make sure filehandle is closed.
+        self._notify_listeners(EVENT_AUDIO_STARTED, desc)
+
+        fh = open(wav_filename, "rb")
+        fh.seek(44)
+        self._input_file_handle = fh
+
+    def stop(self):
+        print("Stopping playing file")
+        if self._input_file_handle is not None:
+            self._input_file_handle.close()
+            self._input_file_handle = None
+        self._notify_listeners(EVENT_AUDIO_FINISHED)
 
     def register_activity_callback(self, callback_func):
         self.activity_listener_callbacks.append(callback_func)
@@ -35,61 +113,6 @@ class AudioPlayer:
         for callback_func in self.activity_listener_callbacks:
             callback_func(event, *args)
 
-    async def _play_file_async(self, wav_filename, desc):
-        _assert_wav_file(wav_filename)
-
-        # Read WAV properties
-        with wave.open(wav_filename) as f:
-            wav_frame_rate = f.getframerate()
-            wav_num_channels = f.getnchannels()
-            wav_sample_width_bits = 8 * f.getsampwidth()
-        
-        # Create I2S for this file
-        audio_out = I2S(
-            self.i2s_id,
-            sck=Pin(self.gpio_sck),
-            ws=Pin(self.gpio_ws),
-            sd=Pin(self.gpio_sd),
-            mode=I2S.TX,
-            bits=wav_sample_width_bits,
-            format=I2S.MONO if wav_num_channels == 1 else I2S.STEREO,
-            rate=wav_frame_rate,
-            ibuf=self.buffer_length_bytes,
-        )
-
-        try :
-            self._notify_listeners(EVENT_AUDIO_STARTED, desc)
-
-            swriter = asyncio.StreamWriter(audio_out)
-            
-            with open(wav_filename, "rb") as wav:
-                _ = wav.seek(44)  # advance to first byte of Data section in WAV file
-
-                while True:
-                    num_read = wav.readinto(self.wav_samples_mv)
-                    if num_read == 0:
-                        # End of file.
-                        break
-                    else:
-                        # apply temporary workaround to eliminate heap allocation in uasyncio Stream class.
-                        # workaround can be removed after acceptance of PR:
-                        #    https://github.com/micropython/micropython/pull/7868
-                        # swriter.write(wav_samples_mv[:num_read])
-                        swriter.out_buf = self.wav_samples_mv[:num_read]
-                        await swriter.drain()
-        finally:
-            # cleanup
-            audio_out.deinit()    
-            self._notify_listeners(EVENT_AUDIO_FINISHED)
-
-    def stop(self):
-        if self._currently_playing_task is not None:
-            self._currently_playing_task.cancel()
-        self._currently_playing_task = None
-
-    def play_file(self, filename, desc=""):
-        self.stop()
-        self._currently_playing_task = asyncio.create_task(self._play_file_async(filename, desc))
 
 
     # # def increase_volume(self):
